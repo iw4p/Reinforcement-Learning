0d68f999df224cfafb56cb761e9a8ea53453ca4b
Amir M. Parvizi <official.parvizi@gmail.com>
2024-04-07 01:36:43 +0430

    Update main.py
    Optimizing the code and adding some things to increase the level of the code as well as more graphical output.

--- 03.Taxi-Driver/main.py
+++ 03.Taxi-Driver/main.py
@@ -1,87 +1,115 @@
-# Imports
 import numpy as np
+import matplotlib.pyplot as plt
 from collections import deque
 
-# Constant
+# Constants
 AGENT = 1
 PASSENGER = 4
 GOAL = 8
 BARRIER = -1
+STREET = 0
 
 # Class & Implementations
 class Map:
-    def __init__(self, length, heigth, barriers, agent, passenger, goal):
+    def __init__(self, length, height, barriers, agent_position, passenger_position, goal_position):
         self.length = length
-        self.heigth = heigth
-        self.map = np.zeros((self.heigth, self.length))
+        self.height = height
+        self.map = np.zeros((self.height, self.length))
         self.barriers = barriers
-        self.agent = agent
-        self.passenger = passenger
-        self.goal = goal
+        self.agent_position = agent_position
+        self.passenger_position = passenger_position
+        self.goal_position = goal_position
 
-    def get_agent(self):
-        return tuple(self.agent)
-    
-    def get_passenger(self):
-        return tuple(self.passenger)
-    
-    def get_goal(self):
-        return tuple(self.goal)
-    
+    def get_agent_position(self):
+        return tuple(self.agent_position)
+
+    def get_passenger_position(self):
+        return tuple(self.passenger_position)
+
+    def get_goal_position(self):
+        return tuple(self.goal_position)
+
     def fill_map(self):
-        if len(self.barriers) != 0:
-            for barrier in self.barriers:
-                self.map[tuple(barrier)] = BARRIER
-        self.map[self.get_agent()] = AGENT
-        self.map[self.get_passenger()] = PASSENGER
-        self.map[self.get_goal()] = GOAL
+        for barrier in self.barriers:
+            self.map[tuple(barrier)] = BARRIER
 
-    def search(self, start, end):
-        queue = deque([[start]])
-        seen = set([start])
-        while queue:
-            path = queue.popleft()
-            x, y = path[-1]
+        self.map[self.get_agent_position()] = AGENT
+        self.map[self.get_passenger_position()] = PASSENGER
+        self.map[self.get_goal_position()] = GOAL
 
-            if self.map[x][y] == end:
-                return path
+    def bfs_search(self, start, goal):
+        frontier = deque([start])
+        came_from = {}
+        came_from[start] = None
 
-            for x2, y2 in ((x+1, y), (x-1, y), (x, y+1), (x, y-1)):
-                if 0 <= x2 < self.length and 0 <= y2 < self.heigth and self.map[x2][y2] != BARRIER and (x2, y2) not in seen:
-                    queue.append(path + [(x2, y2)])
-                    seen.add((x2, y2))
+        while frontier:
+            current = frontier.popleft()
 
-    def convert(self, path, action):
-        out = []
-        for i in range(1, len(path)):
-            current = path[i-1]
-            next = path[i]
-            if (current[0] - next[0] == -1):
-                out.append('right')
-            if (current[0] - next[0] == 1):
-                out.append('left')
-            if (current[1] - next[1] == -1):
-                out.append('down')
-            if (current[1] - next[1] == 1):
-                out.append('up')
-        out.append(action)
-        return out
+            if current == goal:
+                break
 
-    def find_passenger(self):
-        path = self.search(self.get_agent()[::-1], PASSENGER)
-        return self.convert(path, 'pick_up')
+            for dx, dy in [(1, 0), (-1, 0), (0, 1), (0, -1)]:
+                next_x, next_y = current[0] + dx, current[1] + dy
+                if 0 <= next_x < self.length and 0 <= next_y < self.height and self.map[next_y][next_x] != BARRIER:
+                    next_pos = (next_x, next_y)
+                    if next_pos not in came_from:
+                        frontier.append(next_pos)
+                        came_from[next_pos] = current
 
-    def find_goal(self):
-        path = self.search(self.get_passenger()[::-1], GOAL)
-        return self.convert(path, 'leave')
+        if goal not in came_from:
+            print("Error: No path found from", start, "to", goal)
+            return []
+
+        current = goal
+        path = []
+        while current != start:
+            path.append(current)
+            current = came_from[current]
+        path.append(start)
+        path.reverse()
+        return path
+
+    def plot_map(self, path=None):
+        plt.figure(figsize=(10, 10))
+        plt.imshow(self.map, cmap='terrain', interpolation='nearest')
+
+        if path:
+            x, y = zip(*path)
+            plt.plot(y, x, color='red', linewidth=2)
+
+        plt.title('Map with Path')
+        plt.xlabel('Width')
+        plt.ylabel('Height')
+        plt.xticks(range(self.length))
+        plt.yticks(range(self.height))
+        plt.grid(visible=True)
+        plt.show()
 
     def run(self):
         self.fill_map()
-        print(self.map)
-        pick_passenger = self.find_passenger()
-        leave_at_goal = self.find_goal()
-        return (pick_passenger, leave_at_goal)
+        start = self.get_agent_position()[::-1]
+        passenger = self.get_passenger_position()[::-1]
+        goal = self.get_goal_position()[::-1]
+
+        path_to_passenger = self.bfs_search(start, passenger)
+        path_to_goal = self.bfs_search(passenger, goal)
+
+        if path_to_passenger and path_to_goal:
+            print("Path to pick up passenger:", path_to_passenger)
+            print("Path to leave at goal:", path_to_goal)
+
+            self.plot_map(path_to_passenger + path_to_goal[1:])  # Concatenate paths to plot combined path
+        else:
+            print("Failed to find valid path from start to goal.")
+
 
 if __name__ == "__main__":
-    map = Map(3, 3, [], [0,0], [1,1], [2,2])
-    print(map.run())
+    barriers = [
+        (2, 4), (3, 4), (4, 4), (5, 4), (6, 4),
+        (4, 2), (4, 3), (4, 5), (4, 6),
+        (12, 4), (13, 4), (14, 4), (15, 4), (16, 4),
+        (4, 12), (4, 13), (4, 14), (4, 15), (4, 16)
+    ]
+
+    map_instance = Map(20, 20, barriers, [0, 0], [10, 10], [19, 19])
+    map_instance.run()
